d7031098aa22cbc4237909dd52ee5a4d9ed33581
Youngho <cyh6099@gmail.com>
2022-11-03 18:56:09 +0900

    feat : nums가 숫자로만 이루어 졌는지 검사하는 기능 구현

--- src/main/java/baseball/NumsChecker.java
+++ src/main/java/baseball/NumsChecker.java
@@ -1,14 +1,17 @@
 package baseball;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class NumsChecker {
     private final int MAX_NUMS_SIZE = 3;
+    private final String NUMS_REGEX = "^[1-9]*";
+
     public boolean checkIsNonDuplicateNums(List<Integer> nums) {
         return nums.stream()
                 .distinct()
                 .count() == MAX_NUMS_SIZE;
+    }
 
+    public boolean checkIsOnlyDigit(String nums) {
+        return nums.matches(NUMS_REGEX);
     }
 }

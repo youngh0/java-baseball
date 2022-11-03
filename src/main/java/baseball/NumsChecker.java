7baa6f4a03bace234022704a664ac8127d48431e
Youngho <cyh6099@gmail.com>
2022-11-03 20:05:50 +0900

    fix : player입력 유효성 검사 메소드 하나만 외부에 노출하도록 변경.
    별도의 3자리수, 숫자로만 이루어졌는지에 대한 검사는 private으로 변경

--- src/main/java/baseball/NumsChecker.java
+++ src/main/java/baseball/NumsChecker.java
@@ -1,17 +1,30 @@
 package baseball;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class NumsChecker {
     private final int MAX_NUMS_SIZE = 3;
     private final String NUMS_REGEX = "^[1-9]*";
 
-    public boolean checkIsNonDuplicateNums(List<Integer> nums) {
-        return nums.stream()
+    public boolean checkNumsValidation(String nums) {
+        return checkIsOnlyDigit(nums) && checkIsNonDuplicateNums(nums);
+    }
+
+    private boolean checkIsNonDuplicateNums(String nums) {
+        return convertStringToIntegerList(nums).stream()
                 .distinct()
                 .count() == MAX_NUMS_SIZE;
     }
-
-    public boolean checkIsOnlyDigit(String nums) {
+    private boolean checkIsOnlyDigit(String nums) {
         return nums.matches(NUMS_REGEX);
+    }
+
+    private List<Integer> convertStringToIntegerList (String nums) {
+        List<Integer> integerList = new ArrayList<>();
+        for (char num : nums.toCharArray()) {
+            integerList.add(Integer.parseInt(String.valueOf(num)));
+        }
+        return integerList;
     }
 }

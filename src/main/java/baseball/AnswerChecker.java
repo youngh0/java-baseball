0d8649101b365f01ebe39330049bf9a07c278b28
Youngho <cyh6099@gmail.com>
2022-11-03 21:31:01 +0900

    fix : player가 입력하는 답이 3자리인지 확인하는 기능 추가

--- src/main/java/baseball/AnswerChecker.java
+++ src/main/java/baseball/AnswerChecker.java
@@ -3,12 +3,12 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NumsChecker {
+public class AnswerChecker {
     private final int MAX_NUMS_SIZE = 3;
     private final String NUMS_REGEX = "^[1-9]*";
 
-    public boolean checkNumsValidation(String nums) {
-        return checkIsOnlyDigit(nums) && checkIsNonDuplicateNums(nums);
+    public boolean checkAnswerValidation(String nums) {
+        return checkIs3Digits(nums) && checkIsOnlyDigit(nums) && checkIsNonDuplicateNums(nums);
     }
 
     private boolean checkIsNonDuplicateNums(String nums) {
@@ -16,11 +16,16 @@ public class NumsChecker {
                 .distinct()
                 .count() == MAX_NUMS_SIZE;
     }
+
     private boolean checkIsOnlyDigit(String nums) {
         return nums.matches(NUMS_REGEX);
     }
 
-    private List<Integer> convertStringToIntegerList (String nums) {
+    private boolean checkIs3Digits(String num) {
+        return num.length() == 3;
+    }
+
+    private List<Integer> convertStringToIntegerList(String nums) {
         List<Integer> integerList = new ArrayList<>();
         for (char num : nums.toCharArray()) {
             integerList.add(Integer.parseInt(String.valueOf(num)));

983cb1c6c9f8554deeeefffbd23431bc2fa0453d
Youngho <cyh6099@gmail.com>
2022-11-03 20:04:32 +0900

    fix : player입력 유효성검사 캡슐화로 인한 테스트 수정

--- src/test/java/baseball/NumsCheckerTest.java
+++ src/test/java/baseball/NumsCheckerTest.java
@@ -1,77 +1,38 @@
 package baseball;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static camp.nextstep.edu.missionutils.Console.*;
 import static org.assertj.core.api.Assertions.*;
 
-@DisplayName("nums 유효성 검사")
+@DisplayName("player입력 유효성 검사")
 class NumsCheckerTest {
-    @Nested
-    @DisplayName("nums중복 숫자 검사")
-    class DuplicateTest {
-        @Test
-        @DisplayName("3자리 숫자에 중복되는 숫자가 없다면 true")
-        void checkIsNonDuplicateNumsTest() {
-            NumsChecker numsChecker = new NumsChecker();
-            List<Integer> checkData = new ArrayList<>(List.of(1, 3, 2));
-
-            assertThat(numsChecker.checkIsNonDuplicateNums(checkData)).isEqualTo(true);
-        }
-
-        @Test
-        @DisplayName("3자리 숫자에 중복되는 숫자가 있다면 false")
-        void checkIsNonDuplicateNumsExceptionTest() {
-            NumsChecker numsChecker = new NumsChecker();
-            List<Integer> checkData = new ArrayList<>(List.of(1, 3, 3));
-
-            assertThat(numsChecker.checkIsNonDuplicateNums(checkData)).isEqualTo(false);
-        }
-
-        @Test
-        @DisplayName("3자리가 안된다면 false")
-        void checkIsNonDuplicateNumsLessExceptionTest() {
-            NumsChecker numsChecker = new NumsChecker();
-            List<Integer> checkData = new ArrayList<>(List.of(1, 3));
-
-            assertThat(numsChecker.checkIsNonDuplicateNums(checkData)).isEqualTo(false);
-        }
-
-        @Test
-        @DisplayName("3자리가 넘는다면 false")
-        void checkIsNonDuplicateNumsOverExceptionTest() {
-            NumsChecker numsChecker = new NumsChecker();
-            List<Integer> checkData = new ArrayList<>(List.of(1, 3, 4, 9));
-
-            assertThat(numsChecker.checkIsNonDuplicateNums(checkData)).isEqualTo(false);
-        }
+    @Test
+    @DisplayName("player입력은 1~9까지의 서로 다른 3자리 수로 이루어진다.")
+    void checkNumsValidationTest() {
+        NumsChecker numsChecker = new NumsChecker();
+        String verifyData = "132";
+        assertThat(numsChecker.checkNumsValidation(verifyData)).isEqualTo(true);
     }
-
-    @Nested
-    @DisplayName("nums가 숫자로만 이루어졌는지")
-    class OnlyDigitTest {
-        @Test
-        @DisplayName("int로만 이루어진 nums는 true")
-        void checkIsOnlyDigitTest() {
-            NumsChecker numsChecker = new NumsChecker();
-            assertThat(numsChecker.checkIsOnlyDigit("123")).isEqualTo(true);
-
-        }
-
-        @Test
-        @DisplayName("int가 아닌 요소가 포함되면 false")
-        void checkIsOnlyDigitExceptionTest() {
-            NumsChecker numsChecker = new NumsChecker();
-            assertThat(numsChecker.checkIsOnlyDigit("13 ")).isEqualTo(false);
-
-        }
-
+    @Test
+    @DisplayName("player입력이 2자리면 잘못된 입력이다.")
+    void checkNumsValidation2DigitsExceptionTest() {
+        NumsChecker numsChecker = new NumsChecker();
+        String verifyData = "12";
+        assertThat(numsChecker.checkNumsValidation(verifyData)).isEqualTo(false);
+    }
+    @Test
+    @DisplayName("player입력이 4자리면 잘못된 입력이다.")
+    void checkNumsValidation4DigitsExceptionTest() {
+        NumsChecker numsChecker = new NumsChecker();
+        String verifyData = "1247";
+        assertThat(numsChecker.checkNumsValidation(verifyData)).isEqualTo(false);
+    }
+    @Test
+    @DisplayName("player입력에 숫자가 아닌 것이 있으면 잘못된 입력이다.")
+    void checkNumsValidationNotOnlyDigitsExceptionTest() {
+        NumsChecker numsChecker = new NumsChecker();
+        String verifyData = "12 ";
+        assertThat(numsChecker.checkNumsValidation(verifyData)).isEqualTo(false);
     }
 
 

43cd8d194fede37fb63a297203fb761bc44865aa
Youngho <cyh6099@gmail.com>
2022-11-03 18:55:35 +0900

    test : nums가 숫자로만 이루어 졌는지 성공, 예외 unit테스트 작성

--- src/test/java/baseball/NumsCheckerTest.java
+++ src/test/java/baseball/NumsCheckerTest.java
@@ -8,13 +8,14 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.*;
 import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("nums 유효성 검사")
 class NumsCheckerTest {
     @Nested
     @DisplayName("nums중복 숫자 검사")
-    class DuplicateTest{
+    class DuplicateTest {
         @Test
         @DisplayName("3자리 숫자에 중복되는 숫자가 없다면 true")
         void checkIsNonDuplicateNumsTest() {
@@ -23,6 +24,7 @@ class NumsCheckerTest {
 
             assertThat(numsChecker.checkIsNonDuplicateNums(checkData)).isEqualTo(true);
         }
+
         @Test
         @DisplayName("3자리 숫자에 중복되는 숫자가 있다면 false")
         void checkIsNonDuplicateNumsExceptionTest() {
@@ -31,6 +33,7 @@ class NumsCheckerTest {
 
             assertThat(numsChecker.checkIsNonDuplicateNums(checkData)).isEqualTo(false);
         }
+
         @Test
         @DisplayName("3자리가 안된다면 false")
         void checkIsNonDuplicateNumsLessExceptionTest() {
@@ -39,6 +42,7 @@ class NumsCheckerTest {
 
             assertThat(numsChecker.checkIsNonDuplicateNums(checkData)).isEqualTo(false);
         }
+
         @Test
         @DisplayName("3자리가 넘는다면 false")
         void checkIsNonDuplicateNumsOverExceptionTest() {
@@ -47,10 +51,28 @@ class NumsCheckerTest {
 
             assertThat(numsChecker.checkIsNonDuplicateNums(checkData)).isEqualTo(false);
         }
-
     }
 
+    @Nested
+    @DisplayName("nums가 숫자로만 이루어졌는지")
+    class OnlyDigitTest {
+        @Test
+        @DisplayName("int로만 이루어진 nums는 true")
+        void checkIsOnlyDigitTest() {
+            NumsChecker numsChecker = new NumsChecker();
+            assertThat(numsChecker.checkIsOnlyDigit("123")).isEqualTo(true);
 
+        }
+
+        @Test
+        @DisplayName("int가 아닌 요소가 포함되면 false")
+        void checkIsOnlyDigitExceptionTest() {
+            NumsChecker numsChecker = new NumsChecker();
+            assertThat(numsChecker.checkIsOnlyDigit("13 ")).isEqualTo(false);
+
+        }
+
+    }
 
 
 }

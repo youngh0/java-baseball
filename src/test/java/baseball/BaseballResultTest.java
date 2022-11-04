4f4d2fb6961dbfe41eab89148cfa6abb91f03d5e
Youngho <cyh6099@gmail.com>
2022-11-04 23:27:11 +0900

    test : computer, player의 입력값을 비교한 판정결과 출력값 검증 테스트 작성

---
+++ src/test/java/baseball/BaseballResultTest.java
@@ -0,0 +1,60 @@
+package baseball;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+class BaseballResultTest {
+    OutputStream out = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @Test
+    void showResultTest() {
+        List<Integer> computerAnswer = new ArrayList<>(List.of(1,4,5));
+        List<Integer> playerAnswer = new ArrayList<>(List.of(1,4,7));
+
+        BaseballResult baseballResult = new BaseballResult(computerAnswer, playerAnswer);
+        baseballResult.showResult();
+        Assertions.assertThat("2스트라이크").isEqualTo(out.toString().trim());
+    }
+
+    @Test
+    void showResultBallTest() {
+        List<Integer> computerAnswer = new ArrayList<>(List.of(1,4,5));
+        List<Integer> playerAnswer = new ArrayList<>(List.of(3,7,1));
+
+        BaseballResult baseballResult = new BaseballResult(computerAnswer, playerAnswer);
+        baseballResult.showResult();
+        Assertions.assertThat("1볼").isEqualTo(out.toString().trim());
+    }
+
+    @Test
+    void showResultBallStrikeTest() {
+        List<Integer> computerAnswer = new ArrayList<>(List.of(1,4,5));
+        List<Integer> playerAnswer = new ArrayList<>(List.of(1,7,4));
+
+        BaseballResult baseballResult = new BaseballResult(computerAnswer, playerAnswer);
+        baseballResult.showResult();
+        Assertions.assertThat("1볼 1스트라이크").isEqualTo(out.toString().trim());
+    }
+
+    @Test
+    void showResultNothingTest() {
+        List<Integer> computerAnswer = new ArrayList<>(List.of(1,4,5));
+        List<Integer> playerAnswer = new ArrayList<>(List.of(3,7,8));
+
+        BaseballResult baseballResult = new BaseballResult(computerAnswer, playerAnswer);
+        baseballResult.showResult();
+        Assertions.assertThat("낫싱").isEqualTo(out.toString().trim());
+    }
+}

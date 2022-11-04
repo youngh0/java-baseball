28c35f0f9ea0b1fe9a47acb49efb55c704b6a976
Youngho <cyh6099@gmail.com>
2022-11-05 00:10:29 +0900

    feat : 숫자 야구 게임 리플레이 여부에 따른 재시작 or 게임 종료 or 입력 값 예외처리 구현

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,16 +1,45 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class GameController {
+    private final String REPLAY_INPUT_REGEX = "[1-2]{1,1}";
     Computer computer;
+    Player player;
 
     public GameController() {
         computer = new Computer();
+        player = new Player();
     }
 
     public void startGame() {
         List<Integer> computerAnswer = computer.generateRandomNums();
-        System.out.println(computerAnswer);
+        while (true) {
+            List<Integer> integers = player.inputAnswer();
+            BaseballResult baseballResult = new BaseballResult(computerAnswer, integers);
+            baseballResult.showResult();
+            if (baseballResult.isThreeStrike()) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                askReplayBaseball();
+                break;
+            }
+        }
+    }
+
+    private void askReplayBaseball() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String playerInput = Console.readLine();
+        if (!checkReplayInputValidation(Console.readLine())) {
+            throw new IllegalArgumentException();
+        }
+        if (playerInput.equals("1")) {
+            startGame();
+        }
+    }
+
+    private boolean checkReplayInputValidation(String replayInput) {
+        return replayInput.matches(REPLAY_INPUT_REGEX);
     }
 }

a8dbabba622fb5b184153765d3cdd2c0695418f9
Youngho <cyh6099@gmail.com>
2022-11-06 15:48:59 +0900

    test : BaseballNumber생성 시 유효성 검사 및 equals,hashcode오버라이딩에 대한 테스트 작성

--- src/test/java/baseball/domain/BaseballNumberTest.java
+++ src/test/java/baseball/domain/BaseballNumberTest.java
@@ -5,36 +5,43 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("BaseballNumber생성자 유효성, equals테스트")
 class BaseballNumberTest {
-//    @Nested
-//    @DisplayName("BaseballNumber string생성자 예외 테스트")
-//    class baseballNumberStringConstructor {
-//        @Test
-//        @DisplayName("입력이 3글자가 아니면 예외가 발생한다.")
-//        void notThreeLengthExceptionTest() {
-//            Assertions.assertThrows(IllegalArgumentException.class,
-//                    () -> new BaseballNumber("1332"));
-//        }
-//
-//        @Test
-//        @DisplayName("입력에 숫자가 아닌 문자가 있으면 예외가 발생한다.")
-//        void notOnlyDigitsExceptionTest() {
-//            Assertions.assertThrows(IllegalArgumentException.class,
-//                    () -> new BaseballNumber("1a2"));
-//        }
-//
-//        @Test
-//        @DisplayName("입력에 0이 있으면 예외가 발생한다.")
-//        void zeroExceptionTest() {
-//            Assertions.assertThrows(IllegalArgumentException.class,
-//                    () -> new BaseballNumber("102"));
-//        }
-//
-//        @Test
-//        @DisplayName("중복되는 숫자가 있으면 예외가 발생한다.")
-//        void duplicateExceptionTest() {
-//            Assertions.assertThrows(IllegalArgumentException.class,
-//                    () -> new BaseballNumber("133"));
-//        }
-//    }
+    @Nested
+    @DisplayName("1~9이루어져야 한다.")
+    class baseballNumberValidation {
+        @Test
+        void _0은_예외발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> new BaseballNumber(0));
+        }
+
+        @Test
+        void _10은_예외발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> new BaseballNumber(10));
+        }
+    }
+
+    @Nested
+    @DisplayName("equals검사")
+    class baseballNumberStringConstructor {
+        @Test
+        @DisplayName("각각 1,2를 number로 가지는 baseballNumber의 equals는 false다.")
+        void baseballNumberEqualsFalseTest() {
+            BaseballNumber baseballNumber = new BaseballNumber(1);
+            BaseballNumber baseballNumber1 = new BaseballNumber(2);
+            assertThat(baseballNumber.equals(baseballNumber1)).isEqualTo(false);
+        }
+
+        @Test
+        @DisplayName("각각 1,1을 number로 가지는 baseballNumber의 equals는 true다.")
+        void notOnlyDigitsExceptionTest() {
+            BaseballNumber baseballNumber = new BaseballNumber(1);
+            BaseballNumber baseballNumber1 = new BaseballNumber(1);
+            assertThat(baseballNumber.equals(baseballNumber1)).isEqualTo(true);
+        }
+    }
 }

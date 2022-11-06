b1710bf6f7437b298bbb97bba36963ca3667459a
Youngho <cyh6099@gmail.com>
2022-11-06 15:49:32 +0900

    refactor : displayName한글로 변경

--- src/test/java/baseball/BaseballResultTest.java
+++ src/test/java/baseball/BaseballResultTest.java
@@ -4,6 +4,7 @@ import baseball.domain.BaseballNumberList;
 import baseball.domain.BaseballResult;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -11,6 +12,7 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.List;
 
+@DisplayName("게임 결과 출력 및 3스트라이크 여부 테스트")
 class BaseballResultTest {
     OutputStream out = new ByteArrayOutputStream();
 
@@ -20,7 +22,7 @@ class BaseballResultTest {
     }
 
     @Test
-    void showResultTest() {
+    void 쓰리스트라이크_출력_테스트() {
         BaseballNumberList computer = new BaseballNumberList(List.of(1, 2, 3));
         BaseballNumberList player = new BaseballNumberList(List.of(1, 2, 3));
 
@@ -30,7 +32,7 @@ class BaseballResultTest {
     }
 
     @Test
-    void showResultBallTest() {
+    void 볼만_있는_출력_테스트() {
         BaseballNumberList computer = new BaseballNumberList(List.of(1, 2, 3));
         BaseballNumberList player = new BaseballNumberList(List.of(4, 3, 5));
 
@@ -40,7 +42,7 @@ class BaseballResultTest {
     }
 
     @Test
-    void showResultBallStrikeTest() {
+    void 볼_스트라이크_섞인_결과_출력_테스트() {
         BaseballNumberList computer = new BaseballNumberList(List.of(1, 2, 3));
         BaseballNumberList player = new BaseballNumberList(List.of(3, 2, 6));
 
@@ -50,7 +52,7 @@ class BaseballResultTest {
     }
 
     @Test
-    void showResultNothingTest() {
+    void 낫싱_출력_테스트() {
         BaseballNumberList computer = new BaseballNumberList(List.of(1, 2, 3));
         BaseballNumberList player = new BaseballNumberList(List.of(4, 5, 6));
 
@@ -60,7 +62,7 @@ class BaseballResultTest {
     }
 
     @Test
-    void isThreeStrikeTest() {
+    void 쓰리스트라이크_여부_테스트() {
         BaseballNumberList computer = new BaseballNumberList(List.of(1, 2, 3));
         BaseballNumberList player = new BaseballNumberList(List.of(1, 2, 3));
 
@@ -70,7 +72,7 @@ class BaseballResultTest {
     }
 
     @Test
-    void isThreeStrikeFailTest() {
+    void 쓰리스트라이크_false_테스트() {
         BaseballNumberList computer = new BaseballNumberList(List.of(1, 2, 3));
         BaseballNumberList player = new BaseballNumberList(List.of(4, 2, 3));
 
--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -2,52 +2,49 @@ package baseball;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+@DisplayName("Player가 입력하는 정답 유효성 검사")
 class PlayerTest {
     Player player = new Player();
 
-    @Nested
-    @DisplayName("Player정답 입력 예외 테스트")
-    class baseballNumberStringConstructor {
-        @Test
-        @DisplayName("입력이 3글자가 아니면 예외가 발생한다.")
-        void notThreeLengthExceptionTest() {
-            InputStream in = new ByteArrayInputStream("1332".getBytes());
-            System.setIn(in);
-            Assertions.assertThrows(IllegalArgumentException.class,
-                    () -> player.inputAnswer());
-        }
-
-        @Test
-        @DisplayName("입력에 숫자가 아닌 문자가 있으면 예외가 발생한다.")
-        void notOnlyDigitsExceptionTest() {
-            InputStream in = new ByteArrayInputStream("1a2".getBytes());
-            System.setIn(in);
-            Assertions.assertThrows(IllegalArgumentException.class,
-                    () -> player.inputAnswer());
-        }
-
-        @Test
-        @DisplayName("입력에 0이 있으면 예외가 발생한다.")
-        void zeroExceptionTest() {
-            InputStream in = new ByteArrayInputStream("102".getBytes());
-            System.setIn(in);
-            Assertions.assertThrows(IllegalArgumentException.class,
-                    () -> player.inputAnswer());
-        }
-
-        @Test
-        @DisplayName("중복되는 숫자가 있으면 예외가 발생한다.")
-        void duplicateExceptionTest() {
-            InputStream in = new ByteArrayInputStream("133".getBytes());
-            System.setIn(in);
-            Assertions.assertThrows(IllegalArgumentException.class,
-                    () -> player.inputAnswer());
-        }
+    @Test
+    @DisplayName("입력이 3글자가 아니면 예외가 발생한다.")
+    void notThreeLengthExceptionTest() {
+        InputStream in = new ByteArrayInputStream("1332".getBytes());
+        System.setIn(in);
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> player.inputAnswer());
     }
+
+    @Test
+    @DisplayName("입력에 숫자가 아닌 문자가 있으면 예외가 발생한다.")
+    void notOnlyDigitsExceptionTest() {
+        InputStream in = new ByteArrayInputStream("1a2".getBytes());
+        System.setIn(in);
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> player.inputAnswer());
+    }
+
+    @Test
+    @DisplayName("입력에 0이 있으면 예외가 발생한다.")
+    void zeroExceptionTest() {
+        InputStream in = new ByteArrayInputStream("102".getBytes());
+        System.setIn(in);
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> player.inputAnswer());
+    }
+
+    @Test
+    @DisplayName("중복되는 숫자가 있으면 예외가 발생한다.")
+    void duplicateExceptionTest() {
+        InputStream in = new ByteArrayInputStream("133".getBytes());
+        System.setIn(in);
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> player.inputAnswer());
+    }
+
 }

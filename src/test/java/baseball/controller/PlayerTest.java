87928a37fdf8a842ef6f7924d0f5ddd261fef4d4
Youngho <cyh6099@gmail.com>
2022-11-07 20:16:16 +0900

    refactor : inputStream이용하던 테스트 방식을 ApplicationTest에 있는 assertSimpleTest로 변경

--- src/test/java/baseball/controller/PlayerTest.java
+++ src/test/java/baseball/controller/PlayerTest.java
@@ -1,51 +1,57 @@
 package baseball.controller;
 
-import baseball.controller.Player;
+import baseball.Application;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 @DisplayName("Player가 입력하는 정답 유효성 검사")
-class PlayerTest {
-    Player player = new Player();
+class PlayerTest extends NsTest {
 
     @Test
     @DisplayName("입력이 3글자가 아니면 예외가 발생한다.")
     void notThreeLengthExceptionTest() {
-        InputStream in = new ByteArrayInputStream("1332".getBytes());
-        System.setIn(in);
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> player.inputAnswer());
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
     @DisplayName("입력에 숫자가 아닌 문자가 있으면 예외가 발생한다.")
     void notOnlyDigitsExceptionTest() {
-        InputStream in = new ByteArrayInputStream("1a2".getBytes());
-        System.setIn(in);
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> player.inputAnswer());
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
     @DisplayName("입력에 0이 있으면 예외가 발생한다.")
     void zeroExceptionTest() {
-        InputStream in = new ByteArrayInputStream("102".getBytes());
-        System.setIn(in);
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> player.inputAnswer());
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("120"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
     @Test
     @DisplayName("중복되는 숫자가 있으면 예외가 발생한다.")
     void duplicateExceptionTest() {
-        InputStream in = new ByteArrayInputStream("133".getBytes());
-        System.setIn(in);
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> player.inputAnswer());
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("113"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
     }
 
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
 }

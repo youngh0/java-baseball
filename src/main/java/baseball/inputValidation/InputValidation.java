7b4577ccd4ef10966a9626fe23a3e904a0b7e2d4
Youngho <cyh6099@gmail.com>
2022-11-07 23:32:24 +0900

    refactor : 사용자 계속 입력하는지 묻는 입력값에 대한 정규식 변경

--- src/main/java/baseball/inputValidation/InputValidation.java
+++ src/main/java/baseball/inputValidation/InputValidation.java
@@ -3,7 +3,7 @@ package baseball.inputValidation;
 import baseball.errorMessages.IllegalExceptionMessage;
 
 public class InputValidation {
-    private final static String REPLAY_INPUT_REGEX = "[1-2]{1,1}";
+    private final static String REPLAY_INPUT_REGEX = "[1-2]";
 
     public static void validateReplayInputValidation(String replayInput) {
         if (!replayInput.matches(REPLAY_INPUT_REGEX)) {

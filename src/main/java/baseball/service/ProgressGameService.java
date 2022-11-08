b7019968db57546170bc39ce20a67e7074701b03
Youngho <cyh6099@gmail.com>
2022-11-08 14:53:01 +0900

    refactor : 사용자 입력과 컴퓨터 난수간 결과판정하는 로직 BaseballResult로 분리

--- src/main/java/baseball/service/ProgressGameService.java
+++ src/main/java/baseball/service/ProgressGameService.java
@@ -1,9 +1,8 @@
 package baseball.service;
 
+import baseball.repository.BaseballResult;
 import baseball.utils.PlayerInputValidator;
 import baseball.view.InputView;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class ProgressGameService {
@@ -18,49 +17,7 @@ public class ProgressGameService {
         while (!correctAnswer) {
             String playerAnswerInput = InputView.requestPlayerInput();
             List<Integer> playerInputAnswer = playerInputValidator.validatePlayerAnswerInput(playerAnswerInput);
-            List<Integer> baseballResult = judgeBaseballResult(computerAnswer, playerInputAnswer);
-            int strikeCount = baseballResult.get(0);
-            int ballCount = baseballResult.get(1);
-            showResult(strikeCount, ballCount);
-            correctAnswer = isThreeStrike(strikeCount);
+            correctAnswer = new BaseballResult().progressJudgement(computerAnswer, playerInputAnswer);
         }
-    }
-
-    public List<Integer> judgeBaseballResult(List<Integer> computerAnswer, List<Integer> playerAnswer) {
-        List<Integer> result = new ArrayList<>(List.of(0, 0));
-        for (int num = 0; num < 3; num++) {
-            if (computerAnswer.get(num).equals(playerAnswer.get(num))) {
-                result.set(0, result.get(0) + 1);
-                continue;
-            }
-            if (computerAnswer.contains(playerAnswer.get(num))) {
-                result.set(1, result.get(1) + 1);
-            }
-        }
-        return result;
-    }
-
-    public boolean isThreeStrike(int strikeCount) {
-        final String finishGame = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-        final int MAX_STRIKE_COUNT = 3;
-        if (strikeCount == MAX_STRIKE_COUNT) {
-            System.out.println(finishGame);
-            return true;
-        }
-        return false;
-    }
-
-    public void showResult(int strikeCount, int ballCount) {
-        StringBuilder resultString = new StringBuilder();
-        if (ballCount > 0) {
-            resultString.append(ballCount).append("볼 ");
-        }
-        if (strikeCount > 0) {
-            resultString.append(strikeCount).append("스트라이크");
-        }
-        if (ballCount + strikeCount == 0) {
-            resultString.append("낫싱");
-        }
-        System.out.println(resultString);
     }
 }

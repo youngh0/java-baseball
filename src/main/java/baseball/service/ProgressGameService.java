fc4dbd2f9db185a5638ec814b7a6f3819763610b
Youngho <cyh6099@gmail.com>
2022-11-08 15:46:24 +0900

    refactor : 사용자 재시작 로직 ProgressGameService로 분리

--- src/main/java/baseball/service/ProgressGameService.java
+++ src/main/java/baseball/service/ProgressGameService.java
@@ -3,6 +3,7 @@ package baseball.service;
 import baseball.repository.BaseballResult;
 import baseball.utils.PlayerInputValidator;
 import baseball.view.InputView;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
 
@@ -20,5 +21,12 @@ public class ProgressGameService {
             List<Integer> playerInputAnswer = playerInputValidator.validatePlayerAnswerInput(playerAnswerInput);
             correctAnswer = new BaseballResult().progressJudgement(computerAnswer, playerInputAnswer);
         }
+    }
+
+    public boolean endGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String playerReplayInput = Console.readLine();
+        playerInputValidator.validateReplayInputValidation(playerReplayInput);
+        return playerReplayInput.equals("1");
     }
 }

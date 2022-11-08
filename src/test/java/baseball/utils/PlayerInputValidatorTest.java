7a32e3ecbd8305e7dac612fc49cb021df744862b
Youngho <cyh6099@gmail.com>
2022-11-08 16:46:37 +0900

    refactor : @Nested추가

--- src/test/java/baseball/utils/PlayerInputValidatorTest.java
+++ src/test/java/baseball/utils/PlayerInputValidatorTest.java
@@ -9,31 +9,60 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
+@Nested
 class PlayerInputValidatorTest {
     PlayerInputValidator playerInputValidator = new PlayerInputValidator();
-    ProgressGameService progressGameService = new ProgressGameService();
 
-    @Test
-    void 사용자_입력이_유효성_검사를_통과하면_리스트반환() {
-        assertThat(playerInputValidator.validatePlayerAnswerInput("451"))
-                .isEqualTo(List.of(4, 5, 1));
+    @Nested
+    class 사용자_정답_입력_유효성검사{
+        @Test
+        void 사용자_입력이_유효성_검사를_통과하면_리스트반환() {
+            assertThat(playerInputValidator.validatePlayerAnswerInput("451"))
+                    .isEqualTo(List.of(4, 5, 1));
+        }
+
+        @Test
+        void 사용자가_입력한_정답이_세글자가_아니면_예외발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> playerInputValidator.validatePlayerAnswerInput("1234"));
+        }
+
+        @Test
+        void 사용자가_입력한_정답에_문자가_있다면_예외발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> playerInputValidator.validatePlayerAnswerInput("12"));
+        }
+
+        @Test
+        void 사용자가_입력한_정답에_중복숫자가_있으면_예외발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> playerInputValidator.validatePlayerAnswerInput("313"));
+        }
     }
+    @Nested
+    class 사용자_재식작_입력_유효성검사{
+        @Test
+        void 재시작_입력에_두글자_이상이_오면_예외_발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> playerInputValidator.validateReplayInputValidation("1234"));
+        }
 
-    @Test
-    void 사용자가_입력한_정답이_세글자가_아니면_예외발생() {
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> playerInputValidator.validatePlayerAnswerInput("1234"));
-    }
+        @Test
+        void 재시작_입력에_아무것도_안오면_예외_발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> playerInputValidator.validateReplayInputValidation(""));
+        }
 
-    @Test
-    void 사용자가_입력한_정답에_문자가_있다면_예외발생() {
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> playerInputValidator.validatePlayerAnswerInput("12"));
-    }
+        @Test
+        void 재시작_입력에_문자가_오면_예외_발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> playerInputValidator.validateReplayInputValidation(" "));
+        }
 
-    @Test
-    void 사용자가_입력한_정답에_중복숫자가_있으면_예외발생() {
-        Assertions.assertThrows(IllegalArgumentException.class,
-                () -> playerInputValidator.validatePlayerAnswerInput("313"));
+        @Test
+        void 재시작_입력에_1_2를_제외한_숫자가_오면_예외_발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> playerInputValidator.validateReplayInputValidation("3"));
+        }
     }
 }

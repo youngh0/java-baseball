094efc9b634d4f2c2880ad80155723b8864b01cb
Youngho <cyh6099@gmail.com>
2022-11-08 13:02:47 +0900

    feat : 사용자가 입력한 정답 유효성 검사 기능 구현

---
+++ src/main/java/baseball/utils/PlayerInputValidator.java
@@ -0,0 +1,46 @@
+package baseball.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerInputValidator {
+    private final static int PLAYER_ANSWER_CORRECT_SIZE = 3;
+    private final static String PLAYER_ANSWER_REGEX = "^[1-9]*$";
+
+    public List<Integer> validatePlayerAnswerInput(String playerInput) {
+        validateThreeLength(playerInput);
+        validateSatisfiedRange(playerInput);
+        List<Integer> playerAnswerNumbers = convertStringToIntegerList(playerInput);
+        validateIsNonDuplicateNums(playerAnswerNumbers);
+        return playerAnswerNumbers;
+    }
+
+    private List<Integer> convertStringToIntegerList(String nums) {
+        List<Integer> integerList = new ArrayList<>();
+        for (char num : nums.toCharArray()) {
+            integerList.add(Integer.parseInt(String.valueOf(num)));
+        }
+        return integerList;
+    }
+
+    private static void validateThreeLength(String inputNumbers) {
+        if (inputNumbers.length() != PLAYER_ANSWER_CORRECT_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateSatisfiedRange(String inputNumbers) {
+        if (!inputNumbers.matches(PLAYER_ANSWER_REGEX)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateIsNonDuplicateNums(List<Integer> input) {
+        long individualNumberCount = input.stream()
+                .distinct()
+                .count();
+        if (individualNumberCount != PLAYER_ANSWER_CORRECT_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+}

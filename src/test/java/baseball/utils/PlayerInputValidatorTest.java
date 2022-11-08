9b206d7557cb4a0c47d92909d75b774eba84bd1b
Youngho <cyh6099@gmail.com>
2022-11-08 15:51:33 +0900

    test : 사용자 재시작 입력 1,2에 따른 동작 테스트 작성

--- src/test/java/baseball/utils/PlayerInputValidatorTest.java
+++ src/test/java/baseball/utils/PlayerInputValidatorTest.java
@@ -1,6 +1,8 @@
 package baseball.utils;
 
+import baseball.service.ProgressGameService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,6 +11,7 @@ import static org.assertj.core.api.Assertions.*;
 
 class PlayerInputValidatorTest {
     PlayerInputValidator playerInputValidator = new PlayerInputValidator();
+    ProgressGameService progressGameService = new ProgressGameService();
 
     @Test
     void 사용자_입력이_유효성_검사를_통과하면_리스트반환() {
@@ -32,5 +35,41 @@ class PlayerInputValidatorTest {
     void 사용자가_입력한_정답에_중복숫자가_있으면_예외발생() {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> playerInputValidator.validatePlayerAnswerInput("313"));
+    }
+    @Nested
+    class 사용자_재식작_입력_유효성검사{
+        @Test
+        void 재시작_입력에_1이_오면_true() {
+            assertThat(progressGameService.endGame("1")).isEqualTo(true);
+        }
+
+        @Test
+        void 재시작_입력에_2가_오면_false() {
+            assertThat(progressGameService.endGame("2")).isEqualTo(false);
+        }
+
+        @Test
+        void 재시작_입력에_두글자_이상이_오면_예외_발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> playerInputValidator.validateReplayInputValidation("1234"));
+        }
+
+        @Test
+        void 재시작_입력에_아무것도_안오면_예외_발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> playerInputValidator.validateReplayInputValidation(""));
+        }
+
+        @Test
+        void 재시작_입력에_문자가_오면_예외_발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> playerInputValidator.validateReplayInputValidation(" "));
+        }
+
+        @Test
+        void 재시작_입력에_1_2를_제외한_숫자가_오면_예외_발생() {
+            Assertions.assertThrows(IllegalArgumentException.class,
+                    () -> playerInputValidator.validateReplayInputValidation("3"));
+        }
     }
 }

9573730ca7dc674e2bf784a1ed3d40fd93d4df15
Youngho <cyh6099@gmail.com>
2022-11-08 13:03:10 +0900

    feat : 사용자 정답 입력 기능 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,14 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private final static String INPUT_PLAYER_ANSWER = "숫자를 입력해주세요 : ";
+
+    public static String requestPlayerInput() {
+        System.out.print(INPUT_PLAYER_ANSWER);
+        String playerAnswer = Console.readLine();
+        System.out.println();
+        return playerAnswer;
+    }
+}

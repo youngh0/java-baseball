c95adf31b6fb23f34c9728d80deede6b5ed65b54
Youngho <cyh6099@gmail.com>
2022-12-01 03:00:49 +0900

    feat : 게임 결과 출력 기능 구현

---
+++ src/main/java/baseball/domain/BaseballResult.java
@@ -0,0 +1,37 @@
+package baseball.domain;
+
+public class BaseballResult {
+    private final int strikeCount;
+    private final int ballCount;
+
+    public BaseballResult(int strikeCount, int ballCount) {
+        this.strikeCount = strikeCount;
+        this.ballCount = ballCount;
+    }
+
+    public StringBuffer getResultMessage() {
+        StringBuffer result = new StringBuffer();
+        if (strikeCount == 0 && ballCount == 0) {
+            return result.append("낫싱");
+        }
+        result.append(getBallMessage());
+        result.append(getStrikeMessage());
+        return result;
+    }
+
+    private StringBuffer getBallMessage() {
+        StringBuffer ballResult = new StringBuffer();
+        if (ballCount > 0) {
+            return ballResult.append(ballCount).append("볼 ");
+        }
+        return ballResult;
+    }
+
+    private StringBuffer getStrikeMessage() {
+        StringBuffer strikeResult = new StringBuffer();
+        if (strikeCount > 0) {
+            return strikeResult.append(strikeCount).append("스트라이크");
+        }
+        return strikeResult;
+    }
+}

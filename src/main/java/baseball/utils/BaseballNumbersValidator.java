6fdce2b20673cc145dc9c980431fdf3b66d929be
Youngho <cyh6099@gmail.com>
2022-12-01 02:02:38 +0900

    feat : 플레이어의 야구 숫자 입력에 대한 유효성 검사 구현(중복 숫자 확인)

--- src/main/java/baseball/utils/BaseballNumbersValidator.java
+++ src/main/java/baseball/utils/BaseballNumbersValidator.java
@@ -1,8 +1,12 @@
 package baseball.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballNumbersValidator {
     public static void validatePlayerBaseballInput(String baseballInputValue) {
         isOnlyDigit(baseballInputValue);
+        isDuplicateBaseballNumbers(convertToBaseballNumber(baseballInputValue));
     }
 
     private static void isOnlyDigit(String baseballInputValue) {
@@ -11,5 +15,22 @@ public class BaseballNumbersValidator {
             return;
         }
         throw new IllegalArgumentException();
+    }
+
+    private static List<Integer> convertToBaseballNumber(String baseballInputValue) {
+        List<Integer> convertBaseball = new ArrayList<>();
+        for (char baseballNumber : baseballInputValue.toCharArray()) {
+            convertBaseball.add(Integer.parseInt(String.valueOf(baseballNumber)));
+        }
+        return convertBaseball;
+    }
+
+    private static void isDuplicateBaseballNumbers(List<Integer> baseballNumbers) {
+        long nonDuplicateNumberCount = baseballNumbers.stream()
+                .distinct()
+                .count();
+        if (nonDuplicateNumberCount != 3) {
+            throw new IllegalArgumentException();
+        }
     }
 }

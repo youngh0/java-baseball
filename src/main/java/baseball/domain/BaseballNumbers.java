84b53b66130bc17857c900a2e66a4937cfc804f2
Youngho <cyh6099@gmail.com>
2022-12-01 02:17:35 +0900

    feat : 야구게임 3자리 숫자를 가지고 있는 도메인 객체 구현

---
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -0,0 +1,11 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class BaseballNumbers {
+    private List<Integer> baseballNumbers;
+
+    public BaseballNumbers(List<Integer> baseballNumbers) {
+        this.baseballNumbers = baseballNumbers;
+    }
+}
